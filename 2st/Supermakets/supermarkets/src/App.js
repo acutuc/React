df0eeb32d04152ace76ec9ceb19a99b220ebb1ae
acutuc <acutuc94@gmail.com>
2024-02-21 11:52:08 +0100

    Terminar supermarkets

--- 2st/Supermakets/supermarkets/src/App.js
+++ 2st/Supermakets/supermarkets/src/App.js
@@ -2,42 +2,72 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import MapaPersonas from './components/MapaPersonas';
 import TotalPersonas from './components/TotalPersonas';
-import { useState } from 'react';
+import React from 'react';
 
-const App = () => {
 
-  const POBLACION = [
-    [0, 5, 4, 2, 9, 8, 0, 8, 8],
-    [1, 7, 21, 23, 44, 5, 3, 4, 0],
-    [2, 6, 32, 22, 33, 8, 4, 2, 8],
-    [1, 2, 43, 4, 56, 65, 34, 11, 8],
-    [2, 22, 32, 3, 42, 62, 43, 21, 0],
-    [2, 2, 23, 34, 64, 24, 42, 15, 7],
-    [0, 2, 36, 43, 61, 26, 64, 12, 0],
-    [1, 2, 15, 43, 34, 2, 12, 2, 3],
-    [1, 0, 12, 3, 0, 0, 21, 2, 2]
-  ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      poblacion: [
+        [0, 5, 4, 2, 9, 8, 0, 8, 8],
+        [1, 7, 21, 23, 44, 5, 3, 4, 0],
+        [2, 6, 32, 22, 33, 8, 4, 2, 8],
+        [1, 2, 43, 4, 56, 65, 34, 11, 8],
+        [2, 22, 32, 3, 42, 62, 43, 21, 0],
+        [2, 2, 23, 34, 64, 24, 42, 15, 7],
+        [0, 2, 36, 43, 61, 26, 64, 12, 0],
+        [1, 2, 15, 43, 34, 2, 12, 2, 3],
+        [1, 0, 12, 3, 0, 0, 21, 2, 2]
+      ],
+      supermercados: [{
+        //inicializado en componentwillmount
+      }],
+    };
 
-  const [supermercados, setSupermercados] = useState([
-    { fila: null, columna: null }
-  ])
+  }
 
-  console.log(supermercados)
+  componentWillMount = () => {
+    let copiaState = this.state;
+    let arr = [];
+    for (let i = 0; i < copiaState.poblacion.length; i++) {
+      arr.push([]);
+      for (let j = 0; j < copiaState.poblacion[i].length; j++) {
+        arr[i].push({ color: "secondary" })
+      }
+    }
+    copiaState.supermercados = arr;
+    this.setState({ copiaState })
+  }
 
-  return (
-    <>
-      <div className="App">
-        <MapaPersonas
-          poblacion={POBLACION}
-          supermercados = {supermercados}
-          setSupermercados={setSupermercados}
+  setSupermercado = (x, y) => {
+    let copiaState = this.state;
+    if (copiaState.supermercados[x][y].color === "danger") {
+      copiaState.supermercados[x][y].color = "secondary"
+    } else {
+      copiaState.supermercados[x][y].color = "danger"
+    }
+
+    this.setState({ copiaState })
+  }
+
+  render() {
+    
+    return (
+      <>
+        <div className="App">
+          <MapaPersonas
+            poblacion={this.state.poblacion}
+            supermercados={this.state.supermercados}
+            setSupermercado={(x, y) => this.setSupermercado(x, y)}
+          />
+        </div>
+        <TotalPersonas
+          poblacion={this.state.poblacion}
         />
-      </div>
-      <TotalPersonas
-        poblacion={POBLACION}
-      />
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
--- 2st/Supermakets/supermarkets/src/components/MapaPersonas.js
+++ 2st/Supermakets/supermarkets/src/components/MapaPersonas.js
@@ -1,27 +1,27 @@
 import { Table, Button } from 'reactstrap';
 
-const MapaPersonas = ({ poblacion, supermercados, setSupermercados }) => {
-    
+const MapaPersonas = ({ poblacion, supermercados, setSupermercado }) => {
+
     const coordenadas = (x, y) => {
         //x -> fila
         //y -> columna
         console.log(x + " " + y)
-        for(let i = 0; i < poblacion.length; i++){
-            for(let j = 0; j < poblacion[i].length; j++){
-                if(poblacion[i] === x && poblacion[i][j] === y){
-                    console.log("si")
+        
+        for (let i = 0; i < poblacion.length; i++) {
+            for (let j = 0; j < poblacion[i].length; j++) {
+                if (i === x && j === y) {
+                    setSupermercado(x, y)
                 }
             }
         }
-        setSupermercados({ fila: x, columna: y })
     }
-    
+
     return (
         <Table bordered>
             <tbody>
                 {poblacion.map((valorE, indiceE) => {
                     return (<tr>{valorE.map((valorF, indiceF) => {
-                        return (<td><Button style={{ width: 60 }} onClick={() => coordenadas(indiceE, indiceF)}>{valorF}</Button></td>)
+                        return (<td><Button style={{ width: 60 }} onClick={() => coordenadas(indiceE, indiceF)} color={supermercados[indiceE][indiceF].color}>{valorF}</Button></td>)
                     })}</tr>)
                 })}
             </tbody>
